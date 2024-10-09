25f4ac857e649c8d8681a2e46ac20c1e928d2bb1
ArinaTarasova <tarasovaarina07@gmail.com>
2024-10-09 21:40:05 +0300

    Add files via upload

--- prog02.cpp
+++ prog02.cpp
@@ -2,34 +2,131 @@
 #include <cmath>
 using namespace std;
 
-int main () {
-    float H, V, a, g, t, x, y, T, s;
+int main()
+{
+    int n, n1, sumd, dig, opt, p;
+    char cont;
+    do
+    {
+        cout << "\nSelect an option from [1-8] according to the menu:" <<endl;
+        cout << "[1]. Enter a number in the range between 1000 and 999999"<<endl;
+        cout << "[2]. Print the digits of the number"<<endl;
+        cout << "[3]. Count how many digits the number has"<<endl;
+        cout << "[4]. Return the lowest and highest digits of the number"<<endl;
+        cout << "[5]. Return the sum of the digits of the number"<<endl;
+        cout << "[6]. Return the product of the odd digits"<<endl;
+        cout << "[7]. Calculate the inverted version of the number"<<endl;
+        cout << "---------------------------------------------------"<<endl;
+        cout << "Enter option: ";
+        cin>>opt;
+        switch (opt)
+        {
+            case 1: // OPTION 01: Enter the number
+            {
+                do
+                {  
+                    cout<< "Please enter the number ";
+                    cin>>n1;
 
-    cout<<"Please enter H";
-    cin>>H;
-    cout<<"Please enter V";
-    cin>>V;
-    cout<<"Please enter a";
-    cin>>a;
-    cout<<"Please enter t";
-    cin>>t;
-    g = 9.8;
 
-    s = 1 + (2 * g * H) / (V*V * sin(a) * sin(a));
-    if (s < 0) {
-        cout<< "Error";
-    } else 
-    T = V * sin(a) / g * (1 + s);
-    H = 1/2 * (g * T *T) - V * T * sin(a);
-    x = V * t * cos(a);
-    y = H + V * t * sin(a) - 1/2 * g * t * t;
+                }
+                while (n1 < 1000 | n1 > 999999);
+                
+                break;
+                n = n1;
+            };
 
-    if (y <= 0){
-        cout<<"The stone fell to the bottom";
-    } else 
+            case 2: // OPTION 02: Check if the number has repeated digits
+            {
+                cout<< "Please enter the number ";
+                cin>>n;
+                while (n > 0)
+                {
+                    cout<< (n % 10) << " ";
+                    n /= 10;
+                };
+                break;
+            };
 
-    cout<<"Results: x is "<<x<<" y is"<< y;
+            case 3: // OPTION 03: Count how many digits the number has
+            {
+                cout<< "Please enter the number ";
+                cin>>n;
+                dig = 0;
+                while (n > 0)
+                {
+                    dig++;
+                    n /= 10;
+                };
+                cout<< dig << endl;
+                break;
+            };
+
+            case 4: // OPTION 04: Return the lowest and highest digits of the number
+            {
+                cout<< "Please enter the number ";
+                cin>>n;
+                int maxd = -1, mind = 10;
+                while (n > 0)
+                {
+                    if ((n % 10) >= maxd) {maxd = n % 10;};
+                    if ((n % 10) <= mind) {mind = n % 10;};
+                    n /= 10;
+                };
+                cout<<maxd<< endl;
+                cout<<mind<< endl;
+                break;
+            };
+
+            case 5: // OPTION 05: Return the sum of the digits of the number
+            {
+                cout<< "Please enter the number ";
+                cin>>n;
+                sumd = 0;
+                while (n > 0)
+                {
+                    sumd += (n % 10);
+                    n /= 10;
+                };
+                cout<<sumd<< endl;
+                break;
+            };
+
+            case 6: // OPTION 06: Return the product of the odd digits
+            {
+                cout<< "Please enter the number ";
+                cin>>n;
+                p = 1;
+                while (n > 0)
+                {
+                    if ((n % 10) % 2 != 0) {p *= (n % 10);};
+                    n /= 10;
+                };
+                cout<<p<< endl;               
+                break;
+            };
+            
+            case 7: // OPTION 07: Calculate the inverted version of the number
+            {
+                cout<< "Please enter the number ";
+                cin>>n;
+                while (n > 0)
+                {
+                    cout<< (n % 10);
+                    n /= 10;
+                };
+                break;
+            };
+        };
+
+        // Request to user continue or not
+        cout << "\nDo you want continue [Y|N]? "<< endl;
+
+        cin>> cont;
+    }
+    while(cont == 'y' || cont == 'Y');
+
+    cout << "\n The program is over!"<< endl;
 
     return 0;
-
 }
--- prog03.cpp
+++ prog03.cpp
@@ -2,33 +2,122 @@
 #include <cmath>
 using namespace std;
 
-int main () {
-    float x, f, g;
-    cout<<"Please enter x"; 
-    cin>>x;
+int main()
+{
+    int n, opt;
+    double x;
+    char cont;
 
-    if (x <= -4) {
-        cout<<"value should be bigger than -4";
-        return -1;
-    } 
-    if ((x > - 4) && (x < -1)){
-        g = abs(x + 6) / ( abs(x +3) -3); 
-    } else if ((x > -1) && (x < 5)){
-        g = sqrt(5 - x) - 2;
-    } else if(x >= 5){
-        g = log10(x + 10) - 2; 
-    } 
+    do
+    {
+        double result, x1 = x, last_term;
+        cout<< "OPTION 01: Enter the number" << endl;
+        cout<< "OPTION 02: Calculate the McLaren series for the exponential function" << endl;
+        cout<< "OPTION 03: Calculate the McLaren series for the natural logarithm" << endl;
+        cout<< "OPTION 04: Calculate the McLaren series for the sin" << endl;
+        cout<< "OPTION 05: Calculate the McLaren series for the cos" << endl;
+        cout<< "OPTION 06: Calculate the McLaren series for the rational function" << endl;
+        cout<< "----------------------------------------------------------------------" << endl;
+
+        cout<< "\nPlease choose the option";
+        cin>>opt;
+
+        switch (opt)
+        {
+            case 1: 
+            {
+                cout<< "Please enter x ";
+                cin>>x;
+                do
+                {
+                    cout<< "Please enter n from 2 to 20 ";
+                    cin>>n;
+                }
+                while (n < 2 | n > 20);
+                break;
+            };
+
+            case 2: 
+            {
+                last_term = 1; 
+                result = last_term;
+                for (int i = 1; i <= n; i++)
+                {
+                    last_term *= x / i; 
+                    cout<< last_term << " "; 
+                    result += last_term; 
+                };
+                cout<< "e^(x) = " << result; 
+                break;
+            };
+
+            case 3: 
+            {
+                last_term = x; 
+                result = last_term;
+                for (int i = 2; i <= n; i++)
+                {
+                    last_term *= (-1) * x / i; 
+                    cout<< last_term << " "; 
+                    result += last_term; 
+                };
+                cout<< "\nln(1+x) = " << result; 
+                break;
+            };
+
+            case 4: 
+            {
+                last_term = x; 
+                result = last_term;
+                for (int i = 1; i <= n; i++)
+                {
+                    last_term *= (-1) * (x*x) / ((2*i) * (2*i + 1)); 
+                    cout<< last_term << " "; 
+                    result += last_term; 
+                };
+                cout<< "\nsin(x) = " << result; 
+                break;
+            };
+
+            case 5: 
+            {
+                last_term = 1; 
+                result = last_term;
+                for (int i = 1; i <= n; i++)
+                {
+                    last_term *= (-1) * (x*x) / ((2*i - 1) * (2*i)); 
+                    cout<< last_term << " "; 
+                    result += last_term; 
+                };
+                cout<< "\ncos(x) = " << result;
+                break;
+            };
+
+            case 6:
+            {   
+                while (abs(x1) >= 1)
+                {
+                    cout<< "Please enter x, |x| < 1: ";
+                    cin>>x1;
+                };
+                last_term = 1;
+                result = last_term;
+                for (int i = 2; i <= n; i++)
+                {
+                    last_term *= x;
+                    cout<< i * last_term << " ";
+                    result += i * last_term;
+                };
+                cout<< "\n1 / (1 - x)^2 = " << result;
+                break;
+            };
+        }
+
+        cout<< "\nDo you want to continue? [Y/N]";
+        cin>>cont;
+    } while (cont == 'Y');
     
-
-    if (g < -2){
-        f = abs(g / (1 - g));
-    } else if ((g >= -2) && (g <= 1)) {
-        f = pow((g +2), 2); 
-    } else {
-        cout<<"value is out of range";
-    }
-    cout<<"Result:"<<f<< endl; 
-
+    cout<< "The programm is over!" << endl;
 
     return 0;
-}
+};
